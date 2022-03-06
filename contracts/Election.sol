096277b5c65dc2121ce575ca00b90c36c0301b92
i-m-sanju <sanjayk2310btechcse2023@kccitm.edu.in>
2022-03-06 20:15:43 +0530

    added candidates contracts

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -2,8 +2,22 @@ pragma solidity >=0.4.0 <0.8.0;
 
 contract Election {
 
-    string public candidate;
-     constructor () public {
-       candidate = "Candidate 1";
+    struct Candidate {
+        uint id;
+        string name;
+        uint voteCount;
+    }
+    mapping (uint => Candidate) public candidates;
+    
+    uint public candidateCount;
+
+
+    constructor () public {
+       addCandiate("Candidate 1");
+       addCandiate("Candidate 2");
+    }
+    function addCandiate (string memory _name) private{
+        candidateCount ++;
+        candidates[candidateCount]= Candidate(candidateCount, _name, 0); //o is vote count initial
     }
 }
